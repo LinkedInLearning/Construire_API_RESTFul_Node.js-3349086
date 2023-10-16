599396aa7dc8a09e9f062cf354e0fb6a5166cfc2
Sandy Ludosky <sludosky@xebia.fr>
2023-10-16 17:06:36 +0200

    04_02f

--- fichiers-exercice/04/04_02/index.js
+++ fichiers-exercice/04/04_02/index.js
@@ -7,15 +7,17 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-let posts = [];
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/posts", (req, res) => {
-  const data_posts = [...posts, ...data];
-  res.send(data_posts);
+app.get("/posts", async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.send(posts);
+  } catch (e) {
+    res.send(e);
+  }
 });
 
 app.post("/posts/create", (req, res) => {
