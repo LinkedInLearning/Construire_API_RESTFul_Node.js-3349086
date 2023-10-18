10d606457fbef3110f262bfed18269e6c1ceaffa
Sandy Ludosky <sludosky@xebia.fr>
2023-10-18 18:05:42 +0200

    06_02f

--- fichiers-exercice/06/06_02/src/protected_routes/post.js
+++ fichiers-exercice/06/06_02/src/protected_routes/post.js
@@ -1,3 +1,4 @@
+const User = require("../models/User");
 const {
   createPost,
   findOnePostAndUpdate,
@@ -12,6 +13,21 @@ function loginRequired(req, res, next) {
       message: "Unauthorized user, Please register a new account or login",
     });
   }
+  next();
+}
+
+function apiKeyRequired(req, res, next) {
+  const apiKey = req.header("x-api-key");
+  const users = User.find({});
+  const account = users.find((user) => user.apiKey === apiKey);
+
+  if (!apiKey || !account) {
+    return res.status(403).json({
+      code: 403,
+      message: "You are not allowed. Register for a new Api key",
+    });
+  }
+
   next();
 }
 
